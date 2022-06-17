da999506007c277b2a66049019c877a392447d11
Mohamar Rios Flores <mohamar.rios@gmail.com>
2022-06-17 09:05:00 -0600

    cleaned up code

--- server.js
+++ server.js
@@ -11,90 +11,84 @@ const networkInterfaces = os.networkInterfaces();
 const Discord = require('discord.js');
 const client = new Discord.Client();
 
-
+// GPIO libs
 const gpio = require('rpi-gpio');
 const gpiop = gpio.promise;
-const wait = require('util').promisify(setTimeout);
-
-var status = false;
-gpio.on('change', function(channel, value) {
-	//status change 
-	if (value === !status) {
-		status = value;
-		console.log(`Room is ${status ? 'open.' : 'closed.'}`);
-		//382333745769873421 - sum general
-		//475141498778943489 - is the room open
-		client.channels.get('475141498778943489').send(`Room is ${status ? 'open.' : 'closed.'}`);
-	}
-});
-
-
-var ltest = async () => {
-	console.log('start');
-	await gpiop.setup(7, gpio.DIR_IN, gpio.EDGE_BOTH);
-	status = await gpiop.read(7);
-}
-
-ltest();
-
-
 
 //String commands/templates
+const ipAddressV4 = networkInterfaces[config.dev][0]['address'];
+
+function roomStatus() {
+	return `Room is ${status ? 'open.' : 'closed.'}`;
+}
 function logNameID(msg) {
 	return `id: ${msg.author.id} name: ${msg.author.username}`;
 }
 function sshcmd() {
-	return `ssh -i ${config.sshkey} -p ${config.port} ${config.user}@${networkInterfaces[config.dev][0]['address']}`;
+	return `ssh -i ${config.sshkey} -p ${config.port} ${config.user}@${ipAddressV4}`;
 }
 
+var status = false;
 
+
+
+//Light detection status change
+gpio.on('change', function(channel, value) {
+	if (value === !status) {
+		status = value;
+		console.log('pibot:', roomStatus());
+		client.channels.get(config.channel).send(roomStatus());
+	}
+});
+
+
+var setupGPIO = async () => {
+	console.log('pibot:', 'Setting up GPIO');
+	await gpiop.setup(7, gpio.DIR_IN, gpio.EDGE_BOTH);
+	status = await gpiop.read(7);
+}
+
+setupGPIO().catch(console.error);
+
+
+
+//Client event handling
 client.on(
 	'ready',
 	() => {
-		console.log(`pibot: Logged in as ${client.user.tag}!`);
-		// console.log(client.channels)
-		// console.log(client.guilds)
-		// client.channels.get(config.channel).send('henlo meatbags');
+		console.log('pibot:', 'Logged in as', client.user.tag);
 	}
 );
 
 client.on(
 	'message',
 	msg => {
-		// if (msg.content === 'ping') {
-		// 	msg.reply('Pong!');
-		// }
-
-		// if (msg.content === 'bark') {
-		// 	msg.reply(
-		// 		'BARK BARK BARK BARK BARK BARK BARK BARK BARK ' +
-		// 		'BARK BARK BARK BARK BARK BARK BARK BARK BARK ' +
-		// 		'BARK BARK BARK BARK BARK BARK BARK BARK BARK '
-		// 		);
-		// }
-
 		if (msg.channel.type === 'dm' &&
 			(msg.content === 'ip' ||
 			 msg.content === 'iplong' ||
-			 msg.content === 'ssh')
+			 msg.content === 'ssh' ||
+			 msg.content === 'status')
 			) {
 			if (config.admins.includes(msg.author.id)) {
-				console.log(`pibot: ${msg.author.username}(${msg.content})`);
+				console.log('pibot:', msg.author.username, ':', msg.content);
 				if (msg.content === 'ip') {
-					msg.reply(networkInterfaces[config.dev][0]['address']);
+					msg.reply('`' + ipAddressV4 + '`').catch(console.error);
 				}
 				if (msg.content === 'iplong') {
-					msg.reply(JSON.stringify(networkInterfaces));
+					msg.reply('```' + JSON.stringify(networkInterfaces, null, '\t') + '```').catch(console.error);
 				}
 				if (msg.content === 'ssh') {
-					msg.reply(sshcmd());
+					msg.reply('`' + sshcmd() + '`').catch(console.error);
+				}
+				if (msg.content === 'status') {
+					msg.reply(roomStatus()).catch(console.error);
 				}
 			} else {
-				msg.reply('You are not authenticated.');
-				console.log(`pibot: ${logNameID(msg)}`);
+				msg.reply('You are not authenticated');
+				console.log('pibot:', logNameID(msg));
 				client.fetchUser(config.owner)
 					.then(
-						user => user.send(logNameID(msg))
+						owner => owner.send(logNameID(msg))
 					).catch(console.error);
 			}
 		}
@@ -102,18 +96,32 @@ client.on(
 );
 
 client.login(config.discordAPIKey)
- .then(() => console.log('pibot: Started.'))
- .catch(() => console.error('pibot: Crashed.'));
+ .then(() => console.log('pibot:', 'Started'))
+ .catch(() => console.error('pibot:', 'Failed to Start'));
 
 
+
+process.on(
+	'unhandledRejection',
+	(reason, promise) => {
+		console.error('pibot:', reason, 'Unhandled rejection at promise', promise);
+		// process.emit('cleanStop');
+	}
+);
+process.on(
+	'uncaughtException',
+	(err) => {
+		console.error('pibot:', err, 'Uncaught exception');
+		process.emit('cleanStop');
+	}
+);
 process.on('SIGTERM', () => process.emit('cleanStop'));
 process.on('SIGINT', () => process.emit('cleanStop'));
-process.on('uncaughtException', () => process.emit('cleanStop'));
 process.on('SIGUSR1', () => process.emit('cleanStop'));
 process.on('SIGUSR2', () => process.emit('cleanStop'));
 
 process.on('cleanStop', () => {
 	gpio.destroy();
-	console.log('terminated');
+	console.log('pibot:', 'Terminated');
 	process.exit(0);
 });
